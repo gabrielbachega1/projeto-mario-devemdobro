1d6683615fa20cb26cbd8bc4fdb32d9c38cb4b8e
Gabriel Bachega <109998497+gabrielbachega1@users.noreply.github.com>
2023-08-21 10:05:43 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+const botaoTrailer = document.querySelector(".botao-trailer");
+const modal = document.querySelector(".modal");
+const fecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const linkDoVideo = video.src;
+
+function alternarModal(){
+    modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", linkDoVideo);
+});
+
+fecharModal.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "");
+});
